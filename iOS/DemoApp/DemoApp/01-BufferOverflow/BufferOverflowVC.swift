07b41dab93d40c66ae2347fb9fdff9f3d3be0499
nishenlei <nishenlei@huami.com>
2021-03-12 14:49:51 +0800

    feat |> 首页添加demo list

---
+++ iOS/DemoApp/DemoApp/01-BufferOverflow/BufferOverflowVC.swift
@@ -0,0 +1,31 @@
+//
+//  BufferOverflowVC.swift
+//  DemoApp
+//
+//  Created by 倪申雷 on 2021/3/12.
+//
+
+import UIKit
+
+class BufferOverflowVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "Buffer overflow"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- iOS/DemoApp/DemoApp/Main/Home/HomeViewController.swift
+++ iOS/DemoApp/DemoApp/Main/Home/HomeViewController.swift
@@ -6,25 +6,67 @@
 //
 
 import UIKit
+import SnapKit
 
 class HomeViewController: UIViewController {
+    
+    enum SubDemo: String, CaseIterable {
+        case bufferOverflow = "01-BufferOverflow"
+        
+        var vc: UIViewController {
+            switch self {
+            case .bufferOverflow:
+                return BufferOverflowVC()
+            }
+        }
+        
+    }
+    
+    let homeCellID = "homeCellID"
+    
+    lazy var tableView: UITableView = {
+        $0.register(UITableViewCell.self, forCellReuseIdentifier: homeCellID)
+        $0.dataSource = self
+        $0.delegate = self
+        return $0
+    }(UITableView())
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         navigationItem.title = "首页"
-        // Do any additional setup after loading the view.
+        configUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func configUI() {
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { (make) in
+            make.edges.equalTo(view)
+        }
     }
-    */
+    
+}
 
+extension HomeViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        SubDemo.allCases.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: homeCellID)
+        cell?.textLabel?.text = SubDemo.allCases[indexPath.section].rawValue
+        return cell!
+    }
+    
+}
+
+extension HomeViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let targetVC = SubDemo.allCases[indexPath.section].vc
+        targetVC.hidesBottomBarWhenPushed = true
+        navigationController?.pushViewController(targetVC, animated: true)
+    }
+    
 }
